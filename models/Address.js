2aedd36b2c6dce623fa8972658a9a3e78f10478e
Ankitshokeen19 <ankitshokeen001@gmail.com>
2025-09-01 00:22:12 +0530

    Finished Project

--- models/Address.js
+++ models/Address.js
@@ -12,4 +12,4 @@ const addressSchema = new mongoose.Schema({
 
 const Address = mongoose.models.address || mongoose.model('address', addressSchema)
 
-export default Address  
+export default Address
